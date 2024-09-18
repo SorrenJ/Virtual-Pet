003a10d301d7246d9f321040d867d2c8e3822f85
SorrenJ <soalexjao@gmail.com>
2024-09-18 16:49:58 -0700

    add creep vulture species into seed and json

--- server/db/seeds/species_seed.sql
+++ server/db/seeds/species_seed.sql
@@ -3,4 +3,5 @@ INSERT INTO species (species_name, hunger_mod, happy_mod, energy_mod, clean_mod,
 ('Skeletal turtle', 3, 3, 3, 2, 500, 2, 'Herbivore', NULL, 1, NULL),
 ('Bunny ball', 5, 3, 3, 4, 200, 1, 'Carnivore', NULL, 2, NULL),
 ('Fluffy angler', 2, 3, 4, 3, 200, 2, 'Herbivore', NULL, 3, NULL),
-('Cog hybrid', 3, 5, 4, 1, 200, 3, 'Omnivore', NULL, 4, NULL);
+('Cog hybrid', 3, 5, 4, 1, 200, 3, 'Omnivore', NULL, 4, NULL),
+('Creep Vulture', 4, 2, 5, 1, 400, 1, 'Carnivore', NULL, 5, NULL);
