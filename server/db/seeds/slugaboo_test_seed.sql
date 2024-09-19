5e6d703cadb5bfc12d570bd7dafd72e13e2afa21
Krisan Manoharan <krisan1010@hotmail.com>
2024-09-19 15:48:10 -0400

    Create example using Slugaboo

---
+++ server/db/seeds/slugaboo_test_seed.sql
@@ -0,0 +1,2 @@
+INSERT INTO pets (user_id, species_id, name, age, created_at, image, emotion, personality, energy, happiness, hunger, cleanliness)
+(1, 7, "Glurb", 5, CURRENT_TIMESTAMP, monster_assets/slugaboo/blue_default.png, default, "hardy", 100, 100, 100, 100)
