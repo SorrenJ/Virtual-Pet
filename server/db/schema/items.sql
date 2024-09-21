ee9452cd7b8a6b7cba9f44dcf5ed3baeb49a18bc
SorrenJ <soalexjao@gmail.com>
2024-09-21 01:53:05 -0700

    make foreign keys more concise

--- server/db/schema/items.sql
+++ server/db/schema/items.sql
@@ -13,7 +13,7 @@ CREATE TABLE inventory (
   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   user_id INTEGER NOT NULL,
   money NUMERIC,
-  CONSTRAINT fk_user_inventory FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+  user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
 );
 
 -- User food table
@@ -21,12 +21,9 @@ CREATE TABLE user_food (
   id SERIAL PRIMARY KEY,
   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   count INTEGER NOT NULL,
-  user_id INTEGER NOT NULL,
-  inventory_id INTEGER NOT NULL,
-  item_type_id INTEGER NOT NULL,
-  CONSTRAINT fk_user_food_user FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
-  CONSTRAINT fk_user_food_inventory FOREIGN KEY (inventory_id) REFERENCES inventory(id) ON DELETE CASCADE,
-  CONSTRAINT fk_user_food_item_type FOREIGN KEY (item_type_id) REFERENCES foods(id) ON DELETE CASCADE
+  user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+  inventory_id INTEGER REFERENCES inventory(id) ON DELETE CASCADE,
+  item_type_id INTEGER REFERENCES foods(id) ON DELETE CASCADE
 );
 
 -- User toiletries table
@@ -34,12 +31,9 @@ CREATE TABLE user_tolietries (
   id SERIAL PRIMARY KEY,
   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   count INTEGER NOT NULL,
-  user_id INTEGER NOT NULL,
-  inventory_id INTEGER NOT NULL,
-  item_type_id INTEGER NOT NULL,
-  CONSTRAINT fk_user_tolietries_user FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
-  CONSTRAINT fk_user_tolietries_inventory FOREIGN KEY (inventory_id) REFERENCES inventory(id) ON DELETE CASCADE,
-  CONSTRAINT fk_user_tolietries_item_type FOREIGN KEY (item_type_id) REFERENCES tolietries(id) ON DELETE CASCADE
+  user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+  inventory_id INTEGER REFERENCES inventory(id) ON DELETE CASCADE,
+  item_type_id INTEGER REFERENCES tolietries(id) ON DELETE CASCADE
 );
 
 -- User toys table
@@ -47,12 +41,9 @@ CREATE TABLE user_toys (
   id SERIAL PRIMARY KEY,
   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   count INTEGER NOT NULL,
-  user_id INTEGER NOT NULL,
-  inventory_id INTEGER NOT NULL,
-  item_type_id INTEGER NOT NULL,
-  CONSTRAINT fk_user_toys_user FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
-  CONSTRAINT fk_user_toys_inventory FOREIGN KEY (inventory_id) REFERENCES inventory(id) ON DELETE CASCADE,
-  CONSTRAINT fk_user_toys_item_type FOREIGN KEY (item_type_id) REFERENCES toys(id) ON DELETE CASCADE
+  user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+  inventory_id INTEGER REFERENCES inventory(id) ON DELETE CASCADE,
+  item_type_id INTEGER REFERENCES toys(id) ON DELETE CASCADE
 );
 
 -- Shop table
@@ -64,25 +55,24 @@ CREATE TABLE shop (
 -- Toys table
 CREATE TABLE toys (
   id SERIAL PRIMARY KEY,
-  shop_id INTEGER NOT NULL,
   name VARCHAR(255),
   price NUMERIC,
   effects VARCHAR(255),
   description VARCHAR(255),
   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-  CONSTRAINT fk_toys_shop FOREIGN KEY (shop_id) REFERENCES shop(id) ON DELETE CASCADE
+  shop_id INTEGER REFERENCES shop(id) ON DELETE CASCADE,
+  
 );
 
 -- Toiletries table
 CREATE TABLE tolietries (
   id SERIAL PRIMARY KEY,
-  shop_id INTEGER NOT NULL,
   name VARCHAR(255),
   price NUMERIC,
   effects VARCHAR(255),
   description VARCHAR(255),
   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-  CONSTRAINT fk_tolietries_shop FOREIGN KEY (shop_id) REFERENCES shop(id) ON DELETE CASCADE
+  shop_id INTEGER REFERENCES shop(id) ON DELETE CASCADE,
 );
 
 -- Foods table
@@ -94,5 +84,5 @@ CREATE TABLE foods (
   effects VARCHAR(255),
   description VARCHAR(255),
   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-  CONSTRAINT fk_foods_shop FOREIGN KEY (shop_id) REFERENCES shop(id) ON DELETE CASCADE
+  shop_id INTEGER REFERENCES shop(id) ON DELETE CASCADE,
 );
