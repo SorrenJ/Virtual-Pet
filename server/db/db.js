47bd957145d9dcebb3df4e19ac8a26c43297beb3
SorrenJ <soalexjao@gmail.com>
2024-09-22 20:13:21 -0700

    made some edits to the pool file and .env, displayed data on ejs view

--- server/db/db.js
+++ server/db/db.js
@@ -1,11 +1,12 @@
-const Pool = require(pg).Pool;
+const { Pool } = require('pg');
+require('dotenv').config(); // Optional, if using environment variables
 
 const pool = new Pool({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME
+  user: process.env.DB_USER || 'labber',
+  host: process.env.DB_HOST || 'localhost',
+  database: process.env.DB_NAME || 'beastly_bonds_development',
+  password: process.env.DB_PASSWORD || 'labber',  // This should match DB_PASSWORD in .env
+  port: process.env.DB_PORT || 5432, // No space around 5432
 });
 
 module.exports = pool;
