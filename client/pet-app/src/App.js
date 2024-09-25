6389f9aa78bd5c5355f7d45da860045c77b5ad37
Krisan Manoharan <krisan1010@hotmail.com>
2024-09-25 14:00:40 -0400

    Implement react route to App, and connect to a default homePage and shopPage

--- client/pet-app/src/App.js
+++ client/pet-app/src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import ShopPage from './routes/shopPage';
+import HomePage from './routes/homePage'; // Create a simple HomePage component
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <Routes>
+            <Route path="/" element={<HomePage />} /> {/* Home page */}
+            <Route path="/shop" element={<ShopPage />} /> {/* Shop page */}
+          </Routes>
+        </header>
+      </div>
+    </Router>
   );
 }
 
