1dbeb06cb4fbe3acffad177103c9d4c5dfc13639
Krisan Manoharan <krisan1010@hotmail.com>
2024-09-27 14:58:35 -0400

    Update example Glurb by adding sprite, mood, color, and personality id, and creating update time and adopted at

--- server/db/seeds/slugaboo_test_seed.sql
+++ server/db/seeds/slugaboo_test_seed.sql
@@ -1,3 +1,3 @@
-INSERT INTO pets (user_id, species_id, name, age, created_at, image, emotion, personality, energy, happiness, hunger, cleanliness) 
+INSERT INTO pets (user_id, species_id, name, age, adopted_at, sprite_id, mood_id, color_id, personality_id, update_time, energy, happiness, hunger, cleanliness) 
 VALUES
-(1, 7, 'Glurb', 5, CURRENT_TIMESTAMP, 'monster_assets/slugaboo/blue_default.png', 'default', 'hardy', 100, 100, 100, 100);
+(1, 7, 'Glurb', 5, CURRENT_TIMESTAMP, 25, 1, 3, 2, CURRENT_TIMESTAMP, 100, 100, 100, 100);
