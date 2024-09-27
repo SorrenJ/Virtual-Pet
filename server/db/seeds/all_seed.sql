85ce0e2eb76b652e0ae6857ba4c6edb3dfe17710
Krisan Manoharan <krisan1010@hotmail.com>
2024-09-27 14:57:50 -0400

    Remove images and example pet Glurb

--- server/db/seeds/all_seed.sql
+++ server/db/seeds/all_seed.sql
@@ -4,17 +4,6 @@ VALUES
 ('Sorren Jao', 'Poopybuttholee@example.com', 'password123', CURRENT_TIMESTAMP),
 ('Krisan Manoharan', 'Poopybuttholee@example.com', 'password123', CURRENT_TIMESTAMP);
 
--- Insert images first
-INSERT INTO images (image_url) 
-VALUES 
-('image_0_url'),
-('image_1_url'),
-('image_2_url'),
-('image_3_url'),
-('image_4_url'),
-('image_5_url'),
-('https://res.cloudinary.com/deszclhtq/image/upload/v1727112791/Yellow_Default_hrgmsk.png');
-
 -- Then insert species
 INSERT INTO species (species_name, hunger_mod, happy_mod, energy_mod, clean_mod, lifespan, diet_type, diet_desc) 
 VALUES
@@ -25,11 +14,6 @@ VALUES
 ('Cog hybrid', 3, 5, 4, 1, 200, 3, 'Omnivore'),
 ('Creep Vulture', 4, 2, 5, 1, 400, 1, 'Carnivore'),
 ('Slugaboo', 3, 3, 3, 3, 300, 3, 'Omnivore');
-
--- Finally, insert pets
-INSERT INTO pets (user_id, species_id, name, age, created_at, image, emotion, personality, energy, happiness, hunger, cleanliness) 
-VALUES
-(1, 7, 'Glurb', 5, CURRENT_TIMESTAMP, 'monster_assets/slugaboo/blue_default.png', 'default', 'hardy', 100, 100, 100, 100);
 
 
 INSERT INTO shop (created_at) 
