f7523ecd78efd89246966bea984ca8385f079fa4
Krisan Manoharan <krisan1010@hotmail.com>
2024-10-01 18:36:57 -0400

    Add food images to seed

--- server/db/seeds/11_foods_seed.sql
+++ server/db/seeds/11_foods_seed.sql
@@ -1,8 +1,8 @@
 INSERT INTO foods (name, price, effects, food_type, description, shop_id, food_image) VALUES
-('Rotting Banana', 5, 5, 2, '"hello"', 1, 'https://res.cloudinary.com/deszclhtq/image/upload/v1727460483/rotten_banana.jpg'),
-('Salad', 15, 10, 2, '"hello"', 1, 'https://res.cloudinary.com/deszclhtq/image/upload/v1727460483/rotten_banana.jpg'),
-('Can of Beans', 15, 15, 2, '"hello"', 1, 'https://res.cloudinary.com/deszclhtq/image/upload/v1727460483/rotten_banana.jpg'),
-('Chicken Leg', 15, 15, 3, '"hello"', 1, 'https://res.cloudinary.com/deszclhtq/image/upload/v1727460483/rotten_banana.jpg'),
-('Sushi', 30, 20, 3, '"hello"', 1, 'https://res.cloudinary.com/deszclhtq/image/upload/v1727460483/rotten_banana.jpg'),
-('Apple Pie', 30, 20, 2, '"hello"', 1, 'https://res.cloudinary.com/deszclhtq/image/upload/v1727460483/rotten_banana.jpg'),
-('Hamburger', 35, 25, 3, '"hello"', 1, 'https://res.cloudinary.com/deszclhtq/image/upload/v1727460483/rotten_banana.jpg');
+('Rotting Banana', 5, 5, 2, '"hello"', 1, 'https://res.cloudinary.com/deszclhtq/image/upload/v1727812846/food_rotting_banana.png'),
+('Salad', 15, 10, 2, '"hello"', 1, 'https://res.cloudinary.com/deszclhtq/image/upload/v1727812885/food_salad.png'),
+('Can of Beans', 15, 15, 2, '"hello"', 1, 'https://res.cloudinary.com/deszclhtq/image/upload/v1727812838/food_can_of_beans.png'),
+('Chicken Leg', 15, 15, 3, '"hello"', 1, 'https://res.cloudinary.com/deszclhtq/image/upload/v1727812803/food_chicken_leg.png'),
+('Sushi', 30, 20, 3, '"hello"', 1, 'https://res.cloudinary.com/deszclhtq/image/upload/v1727812888/food_sushi.png'),
+('Blueberry Pie', 30, 20, 2, '"hello"', 1, 'https://res.cloudinary.com/deszclhtq/image/upload/v1727812879/food_pie.png'),
+('Hamburger', 35, 25, 3, '"hello"', 1, 'https://res.cloudinary.com/deszclhtq/image/upload/v1727812784/food_hamburger.png');
