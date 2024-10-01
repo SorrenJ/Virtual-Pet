3d507892d06d5ac2efe560a91e3f890a8a2e05d4
Krisan Manoharan <krisan1010@hotmail.com>
2024-10-01 18:38:22 -0400

    Add images for toys

--- server/db/seeds/09_toys_seed.sql
+++ server/db/seeds/09_toys_seed.sql
@@ -1,7 +1,7 @@
 INSERT INTO toys (name, price, effects, description, shop_id, toy_image) VALUES
-('Doll', 100, 50, '"hello"', 1, 'https://res.cloudinary.com/deszclhtq/image/upload/v1727460484/doll.jpg' ),
-('Ball', 50, 25, '"hello"', 1, 'https://res.cloudinary.com/deszclhtq/image/upload/v1727460484/doll.jpg'),
-('iPad', 1000, 100, '"hello"', 1, 'https://res.cloudinary.com/deszclhtq/image/upload/v1727460484/doll.jpg'),
-('Paint  set', 150, 60, '"hello"', 1, 'https://res.cloudinary.com/deszclhtq/image/upload/v1727460484/doll.jpg'),
-('Yoyo', 60, 30, '"hello"', 1, 'https://res.cloudinary.com/deszclhtq/image/upload/v1727460484/doll.jpg'),
-('Rubber Ducky', 40, 20, '"hello" ', 1, 'https://res.cloudinary.com/deszclhtq/image/upload/v1727460484/doll.jpg');
+('Doll', 100, 50, '"hello"', 1, 'https://res.cloudinary.com/deszclhtq/image/upload/v1727812849/toy_doll.png' ),
+('Ball', 50, 25, '"hello"', 1, 'https://res.cloudinary.com/deszclhtq/image/upload/v1727812841/toy_ball.png'),
+('iPad', 1000, 100, '"hello"', 1, 'https://res.cloudinary.com/deszclhtq/image/upload/v1727812767/toy_ipad.png'),
+('Paint  set', 150, 60, '"hello"', 1, 'https://res.cloudinary.com/deszclhtq/image/upload/v1727812873/toy_paint.png'),
+('Yoyo', 60, 30, '"hello"', 1, 'https://res.cloudinary.com/deszclhtq/image/upload/v1727812909/toy_yoyo.png'),
+('Rubber Ducky', 40, 20, '"hello" ', 1, 'https://res.cloudinary.com/deszclhtq/image/upload/v1727812877/toy_duck.png');
