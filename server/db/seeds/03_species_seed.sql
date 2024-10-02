ca740ec320c8fd744a45687f182a0e1a9ca67b76
Krisan Manoharan <krisan1010@hotmail.com>
2024-10-02 14:31:16 -0400

    Add images for Slugaboo and Hippostyx

--- server/db/seeds/03_species_seed.sql
+++ server/db/seeds/03_species_seed.sql
@@ -1,5 +1,5 @@
 -- Then insert species
 INSERT INTO species (species_name, hunger_mod, happy_mod, energy_mod, clean_mod, lifespan, diet_type, diet_desc, image) 
 VALUES
-('Slugaboo', 3, 3, 3, 3, 300, 3, 'Herbivore', 'https://res.cloudinary.com/deszclhtq/image/upload/v1727112791/Slugaboo_Yellow_Default.png');
-('Hippostyx', 1, 5, 4, 2, 300, 3, 'Omnivore', 'https://res.cloudinary.com/deszclhtq/image/upload/v1727813936/Hippostyx_default.gif'),
+('Slugaboo', 3, 3, 3, 3, 300, 3, 'Herbivore', 'https://res.cloudinary.com/deszclhtq/image/upload/v1727816936/Slugaboo_Yellow_Default.png'),
+('Hippostyx', 1, 5, 4, 2, 300, 3, 'Omnivore', 'https://res.cloudinary.com/deszclhtq/image/upload/v1727813936/Hippostyx_default.gif');
