e9a5e4d850256d88fcddb3357a9286f30b4c6e1e
SorrenJ <soalexjao@gmail.com>
2024-10-01 21:59:57 -0700

    home react hompage but fetching routes still not working

--- client/pet-app/src/components/UserFoodTable.jsx
+++ client/pet-app/src/components/UserFoodTable.jsx
@@ -1,44 +1,39 @@
 import React from 'react';
 
 const UserFoodTable = ({ userFood, feedPet, selectedPet }) => {
-
-
-
-    
-  return (
-    <div className="user-data" id="userFoodTable">
-      <h2>User Food Data</h2>
-      <table>
-        <thead>
-          <tr>
-            <th>Food Image</th>
-            <th>Food Name</th>
-            <th>Count</th>
-            <th>Action</th>
-          </tr>
-        </thead>
-        <tbody>
-          {userFood.map((item) => (
-            <tr key={item.item_type_id}>
-              <td>
-                <img src={item.food_image} alt={item.food_name} width="100" />
-              </td>
-              <td>{item.food_name}</td>
-              <td>{item.count}</td>
-              <td>
-                <button
-                  onClick={() => feedPet(selectedPet.pet_id, item.item_type_id)}
-                  disabled={item.count <= 0}
-                >
-                  Feed
-                </button>
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
-  );
+    return (
+        <div className="user-data" id="userFoodTable">
+            <h2>User Food Data</h2>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Food Image</th>
+                        <th>Food Name</th>
+                        <th>Count</th>
+                        <th>Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {userFood.map((item) => (
+                        <tr key={item.item_type_id}>
+                            <td><img src={item.food_image} alt={item.food_name} width="100" /></td>
+                            <td>{item.food_name}</td>
+                            <td>{item.count}</td>
+                            <td>
+                                <button
+                                    onClick={() => feedPet(selectedPet.pet_id, item.item_type_id)}
+                                    disabled={item.count <= 0}
+                                >
+                                    Feed
+                                </button>
+                            </td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </div>
+    );
 };
+
 
 export default UserFoodTable;
