3f88acb689bf9cd98506736ac5017c8f4a87cabc
SorrenJ <soalexjao@gmail.com>
2024-10-03 14:47:01 -0700

    make corrections to queries in clean pet route

--- server/routes/clean_pet_api.js
+++ server/routes/clean_pet_api.js
@@ -8,7 +8,7 @@ router.use(cors());
 
 
 router.post('/', async (req, res) => {
-    const { petId, toiletriesId, toiletriesImage } = req.body; // Get petId and toiletriesId from the request body
+    const { petId, toiletriesId} = req.body; // Get petId and toiletriesId from the request body
     const userId = 1; // Use the actual userId from your session or request
 
     try {
@@ -22,30 +22,39 @@ router.post('/', async (req, res) => {
 
         // Get the toiletries' effect value and count
         const toiletriesQuery = `
-           SELECT t.id AS toiletriesId, effects AS effect, ut.count 
+           SELECT t.id AS toiletriesId, effects AS effect, ut.count, p.cleanliness 
            FROM toiletries t
            JOIN user_toiletries ut ON t.id = ut.item_type_id
+           JOIN pets p ON p.id = $3
            WHERE ut.user_id = $1 AND ut.id = $2;
         `;
-        const toiletriesResult = await pool.query(toiletriesQuery, [userId, toiletriesId]);
+        const toiletriesResult = await pool.query(toiletriesQuery, [userId, toiletriesId, petId]);
 
         if (toiletriesResult.rows.length === 0) {
             return res.status(400).json({ error: 'Toiletry not found or not enough count' });
         }
 
-        const { effect, count } = toiletriesResult.rows[0];
+        let { effect, count, cleanliness } = toiletriesResult.rows[0];
+
+
+        // Ensure effect and hunger are valid numbers (fallback to 0 if not)
+        effect = effect || 0;
+        cleanliness = cleanliness || 0;
+
 
         if (count <= 0) {
             return res.status(400).json({ error: 'No toiletry left to use' });
         }
 
+         // Update the pet's hunger by adding the food's effect, ensuring hunger doesn't exceed 200
+         const newClean = Math.min(cleanliness + effect, 200); // Adjust max hunger to 200
         // Update the pet's cleanliness using the toiletries' effect
         const updateCleanlinessQuery = `
             UPDATE pets
-            SET cleanliness = COALESCE(cleanliness, 0) + $1
+            SET cleanliness = $1
             WHERE id = $2;
         `;
-        await pool.query(updateCleanlinessQuery, [effect, petId]);
+        await pool.query(updateCleanlinessQuery, [newClean , petId]);
 
         // Decrease the toiletries count
         const decreaseToiletryCountQuery = `
