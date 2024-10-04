cf9e40adedab1ab4272d290a7b16de6349a124d3
Krisan Manoharan <krisan1010@hotmail.com>
2024-10-04 13:54:26 -0400

    Create petUpdateMood

---
+++ client/pet-app/src/helper/petUpdateMood.jsx
@@ -0,0 +1,20 @@
+ const updatePetMood = async (userId, petId, moodId) => {
+  try {
+    const response = await fetch(`http://localhost:5000/api/pets/update-image/${userId}/${petId}`, {
+      method: 'PUT',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ mood_id: moodId }),
+    });
+
+    if (!response.ok) {
+      throw new Error('Failed to update mood');
+    }
+
+    const data = await response.json();
+    return data.pet_image_url;  // Return the updated image URL
+  } catch (error) {
+    console.error('Error updating mood:', error);
+  }
+};
+
+export default updatePetMood
