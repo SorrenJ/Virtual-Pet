ced66fdbc4d47a580b1be2fc6c572d3edf7bd4c4
SorrenJ <soalexjao@gmail.com>
2024-10-10 03:39:31 -0700

    interesting loading screens

--- client/pet-app/src/components/loadingScreen.jsx
+++ client/pet-app/src/components/loadingScreen.jsx
@@ -1,12 +1,42 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import '../styles/loadingScreen.scss'; // Ensure you create this CSS file and import it
 
 const LoadingScreen = ({ isTransitioning }) => {
+
+  const [loadingText, setLoadingText] = useState('');
+
+  useEffect(() => {
+    // Define an array of phrases
+    const phrases = [
+      'Take your time ...',
+      'Gotta adopt em all ...',
+      'Dont forget to come to my shop ...',
+      'The real beast is you for not caring for your pet ...',
+      'Daily math training keeps your pet sharp ...',
+      'Just a shoutout to Lighthouse ...'
+    ];
+
+    // Randomly select a phrase
+    const randomPhrase = phrases[Math.floor(Math.random() * phrases.length)];
+
+    // Set the randomly selected phrase
+    setLoadingText(randomPhrase);
+  }, []); // Empty dependency array means this effect will only run once when the component mounts
+
+
   return (
     <div className={`loading-screen ${isTransitioning ? 'swipe-out' : ''}`}>
       <div className="loading-content">
+      <div className='lil-shopkeeper'>
+      <img
+          src='https://res.cloudinary.com/deszclhtq/image/upload/v1728147784/Shopkeeper_Neutral_grmlj6.png'
+          alt="shopkeeper"
+          className='shopkeeper'
+        />
+        </div>
+        <h1 className="loading-text">{loadingText}</h1>
+
         <div className="spinner"></div>
-        <h1>Loading...</h1>
       </div>
     </div>
   );
